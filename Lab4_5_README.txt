414bd494f8f6d832f64bc1938787e22ebacf5559
pgj1 <pgj1@users.noreply.github.com>
2020-07-30 09:24:07 -0500

    Add project files.

---
+++ Lab4_5_README.txt
@@ -0,0 +1,36 @@
+﻿LAB: The Trivia Website Leaderboard
+Lesson Time: 60 Minutes
+
+In this lab, you will use List and ForEach loops to practice working with collections.
+
+Scenario:
+You work for a company that runs a trivia web site. Users of the site compete by answering trivia 
+questions to get the best score possible. One of the features of the site is that users are able 
+to see a list of high scores.
+
+You’ve been asked to use C# to create the score leaderboard logic. You work for a large team, 
+so someone else will worry about getting it to display on the page, your task is to implement the code logic.
+
+You’ve been given these scores to work with.
+Score
+4400
+8900
+1200
+3700
+7200
+
+Using C# lists, do the following.
+1. Create a list that holds the scores you’ve been given.
+
+2. Sort the scores lowest to highest and output to the screen.
+
+3. Sort the scores highest to lowest and output to the screen.
+
+4. You’ve just been given a new score that wasn’t part of the original list. Add the score 2400 to the list.
+
+5. A user has closed their account and needs to have all of their information deleted. 
+   Remove the score 4400 from the list.
+
+
+To view our solution to the Lab, view the “4_5LABSolution” project from our GitHub.
+https://github.com/ACTWebDev/SD104/blob/master/4_5LabSolution/4_5LabSolution/Program.cs
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab4_5_TriviaLeaderboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 4.5 Trivia Leaderboard \n");
+
+            //create generic list and add values
+            List<int> tList = new List<int>();
+            tList.Add(4400);
+            tList.Add(8900);
+            tList.Add(1200);
+            tList.Add(3700);
+            tList.Add(7200);
+            
+            //print values as added to list 
+            Console.WriteLine(" *Unordered list");
+            foreach (int score in tList)
+            {
+                Console.WriteLine(score);
+            }
+
+            //sort the list in default order and print it.
+            tList.Sort();
+            Console.WriteLine("\n *Default Order List");
+            foreach (int score in tList)
+            {
+                Console.WriteLine(score);
+            }
+
+            //sort the list in reverse order and print it.
+            tList.Reverse();
+            Console.WriteLine("\n *Reverse Order List");
+            foreach (int score in tList)
+            {
+                Console.WriteLine(score);
+            }
+
+            //Add new score and print list in descending order 
+            tList.Add(2400);
+            tList.Sort();
+            tList.Reverse();
+            Console.WriteLine("\n *Reverse Order List with new score 2400 added");
+            foreach (int score in tList)
+            {
+                Console.WriteLine(score);
+            }
+
+            //Remove the score 4400 from the list.
+            tList.Remove(4400);
+            Console.WriteLine("\n *Reverse Order List with score 4400 removed");
+            foreach (int score in tList)
+            {
+                Console.WriteLine(score);
+            }
+
+
+        }
+    }
+}
